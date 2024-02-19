ac54a0877290b23fd501ec66700a88c71fac2ca7
Ian Guinn <guinnis@ornl.gov>
2024-02-19 18:36:31 -0500

    Fixed bug with fftw

--- src/dspeed/processing_chain.py
+++ src/dspeed/processing_chain.py
@@ -1406,29 +1406,14 @@ class ProcessorManager:
                 )
 
                 # reshape just in case there are some missing dimensions
-                arshape = list(param.shape)
-                # check of shape mismatch is just due to wf buffer dim being included
-                # this is the case for some factory functions e.g. fft
-                if (
-                    len(shape) == len(arshape) + 1
-                    and shape[0] == self.proc_chain._block_width
-                    and dim_list[0].grid is None
-                ):
-                    pass
-                else:
-                    for idim in range(-1, -1 - len(shape), -1):
-                        if (
-                            len(arshape) < len(shape) + 1 + idim
-                            or arshape[idim] != shape[idim]
-                        ):
-                            arshape.insert(len(arshape) + idim + 1, 1)
-
-                if param.is_const:
-                    param = param.get_buffer(grid).reshape(arshape)
-                else:
-                    param = param.get_buffer(grid).reshape(
-                        tuple([self.proc_chain._block_width] + arshape)
-                    )
+                arshape = list(param.buffer.shape)
+                for idim in range(-1, -1 - len(shape), -1):
+                    if (
+                        len(arshape) < -idim
+                        or arshape[idim] != shape[idim]
+                    ):
+                        arshape.insert(len(arshape) + idim + 1, 1)
+                param = param.get_buffer(grid).reshape(arshape)
 
             elif isinstance(param, str):
                 # Convert string into integer buffer if appropriate
